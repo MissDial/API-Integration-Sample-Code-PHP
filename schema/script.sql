fe78d35c4e62fe3fbf8a2a5c5f6ce7a6733c35ed
Cozy Vision Technologies Pvt. Ltd <prashant@cozyvision.com>
2016-06-16 19:26:06 +0530

    Update script.sql

--- schema/script.sql
+++ schema/script.sql
@@ -1,9 +1,9 @@
 CREATE TABLE missdial (
-id INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-number VARCHAR(12) NOT NULL,
-datetime TIMESTAMP,
-circle VARCHAR(30),
-operator VARCHAR(50),
-ndnd VARCHAR(13),
-prefix VARCHAR(7)
+`id` INT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+`number` VARCHAR(12) NOT NULL,
+`datetime` TIMESTAMP,
+`circle` VARCHAR(30),
+`operator` VARCHAR(2),
+`ndnd` tinyint(1),
+`prefix` VARCHAR(4)
 );
